bb9205d4e60c26fadb9584ba724e1c9a6126775b
bpc50 <38820740+bpc50@users.noreply.github.com>
2018-06-21 23:26:51 -0400

    and more

--- 02_Packages/stringUtil/name.go
+++ 02_Packages/stringUtil/name.go
@@ -1,4 +1,4 @@
 package stringUtil
 
-var MyName = "BPCizzle" //if the variable name begins with upper case, it can be called by the main.go executable in another folder.
-//var myname = "BPC" //if the var naem begins with lower case, it can only be called from a main.go executable within the same folder.
+var MyName = "BPCizzle" //if the variable name begins with upper case, it can be called by a package/function in another folder.
+//var myname = "BPC" //if the var name begins with lower case, it can only be called from another package/function within the same folder.
