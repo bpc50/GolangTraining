4e977da906f0234e574fbfe482ebd9137e69d977
bpc50 <38820740+bpc50@users.noreply.github.com>
2018-06-21 23:18:50 -0400

    more package testing

--- 02_Packages/main/main.go
+++ 02_Packages/main/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	"github.com/bpc50/02_Packages/stringUtil"
+	"github.com/bpc50/GolangTraining/02_Packages/stringUtil"
 )
 
 func main() {
-	fmt.Println(stringUtil.MyName)
+	fmt.Println(stringUtil.MyName) //grab the folder/package "stringUtil", and the variable "MyName" from one of the files (name.go) within it.
 }
--- 02_Packages/stringUtil/name.go
+++ 02_Packages/stringUtil/name.go
@@ -1,3 +1,4 @@
 package stringUtil
 
-var MyName = "BPC"
+var MyName = "BPCizzle" //if the variable name begins with upper case, it can be called by the main.go executable in another folder.
+//var myname = "BPC" //if the var naem begins with lower case, it can only be called from a main.go executable within the same folder.
