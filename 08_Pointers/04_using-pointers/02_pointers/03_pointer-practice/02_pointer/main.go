12c407d4c014a5e41fe39e0d550caf156d2c3aaf
bpc50 <38820740+bpc50@users.noreply.github.com>
2018-07-19 22:25:53 -0400

    7/19 - pointer prac

--- 08_Pointers/04_using-pointers/02_pointers/03_pointer-practice/02_pointer/main.go
+++ 08_Pointers/04_using-pointers/02_pointers/03_pointer-practice/02_pointer/main.go
@@ -9,6 +9,6 @@ func bpc(b *int) { // 2) bpc is now passed up here, and then stored in new var b
 
 func main() {
 	a := 100
-	bpc(&a)        // 1) bpc is passed value of variable a.
+	bpc(&a)        // 1) bpc is passed memory address (which remember, also holds the value, though not shown) of variable a.
 	fmt.Println(a) // 4) will ALSO be 200, cause changing the value, after dereferencing from memory address, changes orig var value
 }
