2b9b328037b27f0ebf4556c2f36df2e5af0eb69e
bpc50 <38820740+bpc50@users.noreply.github.com>
2018-08-13 22:49:56 -0400

    8/13 - more loop prac

--- 10_Loops/02_nested-loops/main.go
+++ 10_Loops/02_nested-loops/main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i < 3; i++ { //This is outside.
-		for j := 0; j < 3; j++ { //This is nested, inside. Each time outside runs once, this runs thru all conditions.
-			fmt.Println(i, " - ", j)
+	for i := 0; i < 2; i++ { //This is outside.
+		for j := 0; j < 4; j++ { //This is nested, inside. Each time outside runs once, this runs thru all conditions.
+			fmt.Println("Outer", i, " - ", "Inner", j)
 		}
 	}
 }
