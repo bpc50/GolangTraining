feaa3baba5c909735248609b55b8e81465654c78
bpc50 <38820740+bpc50@users.noreply.github.com>
2018-08-22 21:24:45 -0400

    8/22 - rune to string conversions

--- 99_Extra-Stuff/Rune/main.go
+++ 99_Extra-Stuff/Rune/main.go
@@ -4,6 +4,14 @@ import "fmt"
 
 func main() {
 	for i := 47; i <= 70; i++ {
-		fmt.Printf("%v - %v - %v \n", i, string(i), []byte(string(i)))
+		fmt.Printf("%v - %v - %v \n", i, string(i), []byte(string(i))) //Rune is a character, and a string is a collection of Rune's.
 	}
+
+	foo := 'a' //foo is initialized as the string (ASCII) value of 'a', which is 97.  Different than "a", which is literal value, ie, type string.
+	fmt.Println(foo)
+	fmt.Printf("%T \n", foo)       //print the type.
+	fmt.Printf("%T \n", rune(foo)) //print the type, after converting the foo type to a rune. rune is just an alias for int32, so it's the same.
 }
+
+//Rune:  above we used "a slice of byte, ie, []byte(string(i))" to print the Rune value of i.
+//So in each loop iteratio we printed 1) loop counter value 2) the string value of that 3) the rune value of that.
---
+++ 99_Extra-Stuff/String/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	foo := 'a'
+	fmt.Println(foo)
+	fmt.Println(string(foo)) //This is how you convert a variable into it's string value, simply (string(foo))
+}
